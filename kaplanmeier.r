d114cfd5eb23d3b1a215ebabcdcc5c5a14ea6332
Drashti <Drashti.Sikligar@americanhipinstitute.org>
2023-10-30 15:35:31 -0500

    Using a different library for KM analysis to create a nicer plot"

--- kaplanmeier.r
+++ kaplanmeier.r
@@ -1,5 +1,12 @@
+#Kaplan Meier curve creator
+#dataframe should be follow up time and events
+#install packages you don't have by copy pasting install packages line into the console
+
+#install.packages('ggsurvfit', 'survival', 'ggplot2')
+
 library(survival)
 library(ggplot2)
+library(ggsurvfit)
 
 df <- read.csv(file = '5yrdataTHA.csv', fileEncoding = 'UTF-8-BOM')
 
@@ -8,5 +15,13 @@ km <- with(df, Surv(FU.time, Event))
 head(km, 28)
 
 km_fit <- survfit(Surv(FU.time, Event) ~ 1, data = df)
+survfit2(Surv(FU.time, Event) ~ 1, data = df) %>% 
+  ggsurvfit() +
+  labs(
+    x = "Months",
+    y = "Overall survival probability"
+    title = 'Time to THA on PAO patients'
+  ) + 
+  add_confidence_interval()
 
-plot(km_fit, xlab = 'months', main = 'kaplan meyer plot')
+#plot(km_fit, xlab = 'months', ylab = 'survival probability', main = 'kaplan meyer plot')
